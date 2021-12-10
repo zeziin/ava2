32069ee9f3e575dd4a36bce4e1cd001a88ea3e9a
zeziin <jose.bene.tava@gmail.com>
2021-12-10 12:48:46 -0300

    ava2

---
+++ ava2.js
@@ -0,0 +1,13 @@
+let n_alunos = 10;
+
+for (let n = 0; n <= n_alunos; n++) {
+    if(n==0){
+        console.log(n,",esse número é o zero")
+    }
+    else if(n%2==0){
+        console.log(n,",esse número é par")
+    }
+    else{
+        console.log(n,",esse número é ímpar")
+    }
+}
